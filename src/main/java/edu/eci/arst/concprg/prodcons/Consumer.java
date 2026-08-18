49188300504a683b366e3b002b9ec486ce78220e
SebasAuM <Aasebastiann@gmail.com>
2026-08-18 18:04:16 -0500

    Parte1_punto2 en Consumer

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -11,24 +11,29 @@ import java.util.Queue;
  *
  * @author hcadavid
  */
-public class Consumer extends Thread{
-    
+public class Consumer extends Thread {
+
     private Queue<Integer> queue;
-    
-    
-    public Consumer(Queue<Integer> queue){
-        this.queue=queue;        
+
+    public Consumer(Queue<Integer> queue) {
+        this.queue = queue;
     }
-    
+
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+            synchronized (queue) {
+                while (queue.isEmpty()) {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                int elem = queue.poll();
+                System.out.println("Consumer consumes " + elem);
+                queue.notifyAll();
             }
-            
         }
     }
 }
